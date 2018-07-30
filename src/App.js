52a4a5b79bc0ac78a5f7e76d4c25556eeac5c4cb
Salvatore Santagati <salvatore.santagati@gmail.com>
2018-07-30 16:45:35 +0200

    refactor: separate data from component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
 import LocalMap from './LocalMap'
 import LocalFilter from './LocalFilter'
+import cataniaLocations from './Catania.json'
 import './App.css';
 
 
 
 class App extends Component {
+
   state = {
-          locations : [
-           {title: 'Piazza Duomo', location: {lat: 37.502496, lng: 15.087076}},
-           {title: 'Stadio Angelo Massimino', location: {lat: 37.515807, lng: 15.071064}},
-           {title: 'Teatro Massimo Bellini', location: {lat: 37.504207, lng: 15.090006}},
-           {title: 'Aeroporto', location: {lat: 37.467305, lng: 15.065775}},
-           {title: 'ASD Scacchi Catania', location: {lat: 37.523458, lng: 15.067489}}
-         ]
+          currentLocations : cataniaLocations,
+          selectedLocation: {}
   }
+
   render() {
+    const { locations } = this.state;
+    console.log( locations);
     return (
       <div className="App">
-        
+
       <div className="filter">
         <LocalFilter
-         locations={this.state.locations}
+         locations={this.state.currentLocations}
+         selectedLocation={this.state.selectedLocation}
         />
       </div>
      <div className="map">
        <LocalMap
-          locations={this.state.locations}
+          locations={this.state.currentLocations}
          />
         </div>
       </div>
